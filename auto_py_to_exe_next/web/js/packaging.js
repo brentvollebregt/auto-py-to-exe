829f461df01d1368736f8d4ac13033b633969dd5
Brent Vollebregt <brent.vollebregt@gmail.com>
2020-05-04 17:33:43 +1200

    Cleaned up packaging state styles

--- auto_py_to_exe_next/web/js/packaging.js
+++ auto_py_to_exe_next/web/js/packaging.js
@@ -5,46 +5,39 @@ const setPackagingState = (newState) => {
 
     const outputSectionNode = document.getElementById('output');
     const outputTextAreaNode = outputSectionNode.querySelector('textarea');
-    const mainButtonsWrapperNode = document.getElementById('package-button-wrapper');
     const convertButtonNode = document.getElementById('package-button');
     const openOutputButtonNode = document.getElementById('open-output-folder-button');
     const commonIssueLinkNode = document.getElementById('common-issue-link');
 
-    // TODO Convert some of this to classes
     switch (newState) {
         case PACKAGING_STATE_READY:
             // Clear output
-            outputSectionNode.style.display = 'none';
+            outputSectionNode.classList.remove('show');
             outputTextAreaNode.value = '';
             outputTextAreaNode.rows = 0;
+            outputTextAreaNode.classList.remove('failure');
             // Set the main button back to initial value
             convertButtonNode.innerHTML = "Convert .py to .exe";
             // Hide open folder button
-            mainButtonsWrapperNode.style.display = 'block';
-            openOutputButtonNode.style.display = 'none';
+            openOutputButtonNode.classList.remove('show');
             // Hide common issue link
-            commonIssueLinkNode.style.display = 'none';
+            commonIssueLinkNode.classList.remove('show');
             return;
         case PACKAGING_STATE_PACKAGING:
             // Disable convert button
-            convertButtonNode.style.filter = 'grayscale(1)';
-            convertButtonNode.style.cursor = 'not-allowed';
+            convertButtonNode.disabled = true;
             convertButtonNode.innerHTML = "Converting...";
             // Show output
-            outputSectionNode.style.display = 'block';
+            outputSectionNode.classList.add('show');
             return;
         case PACKAGING_STATE_COMPLETE:
             // Re-enable convert button and re-purpose it
-            convertButtonNode.style.cursor = '';
-            convertButtonNode.style.filter = '';
+            convertButtonNode.disabled = false;
             convertButtonNode.innerHTML = "Clear Output";
             // Show open folder button (beside "Clear Output" button)
-            mainButtonsWrapperNode.style.display = 'grid';
-            mainButtonsWrapperNode.style.gridGap = '4px';
-            mainButtonsWrapperNode.style.gridTemplateColumns = '1fr 1fr';
-            openOutputButtonNode.style.display = 'block';
+            openOutputButtonNode.classList.add('show');
             // Show common issue link
-            commonIssueLinkNode.style.display = 'block';
+            commonIssueLinkNode.classList.add('show');
             return;
     }
 };
@@ -56,4 +49,10 @@ const startPackaging = () => {
 
 const setPackagingComplete = (successful) => {
     setPackagingState(PACKAGING_STATE_COMPLETE);
+
+    // Show red border around output on failure
+    if (!successful) {
+        const outputTextAreaNode = document.querySelector('#output textarea');
+        outputTextAreaNode.classList.add('failure');
+    }
 };
